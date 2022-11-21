e21304c6db893b58d6908776f43a5f9550bf7894
Francesco Ratti <francesco2.ratti@mail.polimi.it>
2022-11-21 21:50:57 +0100

    finished python script to explore design space and generate large json with data

---
+++ src/DSE_generate_json.py
@@ -0,0 +1,75 @@
+import subprocess
+import numpy
+import os
+import math
+import queue
+
+def main():
+    try:
+        os.remove('dse.json')
+    except Exception:
+        pass
+
+
+    fl=open('dse.json', 'wb')
+
+    #expRegion=7
+    expRegion=2
+    expRegionMultiplier=1
+
+    #expTrain=10
+    expTrain=2
+    expTrainMultiplier=100
+
+    executions=50000
+
+    workers_num=8
+
+    #procArr = numpy.full(-1, arrSize)
+    procArr=queue.Queue(workers_num)
+    fl.write(b'[')
+
+    first=True
+    for t in range(1, expTrain+1, 1):
+        for r in range(1, expRegion+1, 1):
+            reg=math.exp(r)*expRegionMultiplier
+            train=math.exp(t)*expTrainMultiplier
+
+            procArr.put(subprocess.Popen(["%userprofile%/eclipse-workspace/faultdetector_benchmark/Debug/faultdetector_benchmark.exe", "-r", str(int(reg)), "-t", str(int(train)), "-e", str(int(executions))], shell=True, stdout=subprocess.PIPE))
+            
+            if (procArr.full()):
+                if (first):
+                    first=False
+                else:
+                    fl.write(b',')
+                fl.write(procArr.get().stdout.read())
+
+    while (not procArr.empty()):
+        if (first):
+            first=False
+        else:
+            fl.write(b',')
+        fl.write(procArr.get().stdout.read())
+    
+    fl.write(b']')
+    fl.flush()
+    fl.close()
+
+
+""" def maina():
+    os.remove('pippo.txt')
+    fl=open('pippo.txt', 'a')
+    fl.write('[')
+    fl.flush()
+    faultdet=subprocess.Popen('%userprofile%/eclipse-workspace/faultdetector_benchmark/Debug/faultdetector_benchmark.exe', shell=True, stdout=fl)
+    subprocess.Popen.wait(faultdet)
+    fl.write(',')
+    fl.flush()
+    faultdet=subprocess.Popen('%userprofile%/eclipse-workspace/faultdetector_benchmark/Debug/faultdetector_benchmark.exe', shell=True, stdout=fl)
+    subprocess.Popen.wait(faultdet)
+    fl.write(']')
+    fl.flush()
+    fl.close() """
+
+if __name__ == "__main__":
+    main()
